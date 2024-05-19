e4b8704c195e8299ffc220f7924decffb33afeb0
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-05-19 15:01:09 +0300

    ff

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -15,24 +15,57 @@ const Products = ({ cat, filters, sort }) => {
 
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFilteredProducts] = useState([]);
+    const BASE_URL = "http://localhost:5050/api/products";
 
     useEffect(() => {
         const getProducts = async () => {
             try {
-                const res = axios.get()
+                const res = await axios.get(cat ? `${BASE_URL}?category=${cat}` : `${BASE_URL}`);
+                const data = res.data.products;
+                console.log(data);
+                setProducts(data);
             } catch (error) {
                 console.log(error.message);
             }
-        };
+        }
         getProducts();
     }, [cat]);
 
+    useEffect(() => {
+        cat &&
+            setFilteredProducts(
+                products.filter(item =>
+                    Object.entries(filters).every(([key, value]) =>
+                        item[key].includes(value)
+                    )
+                )
+            );
+    }, [products, cat, filters]);
+
+    useEffect(() => {
+        if (sort === "newest") {
+            setFilteredProducts(prev =>
+                [...prev].sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
+            );
+        } else if (sort === "asc") {
+            setFilteredProducts(prev =>
+                [...prev].sort((a, b) => (a.price > b.price ? 1 : -1))
+            );
+        } else {
+            setFilteredProducts(prev =>
+                [...prev].sort((a, b) => (a.price > b.price ? -1 : 1))
+            );
+        }
+    }, [sort])
 
     return (
         <Container>
-            {popularProducts.map(item => (
+
+            {cat ? filteredProducts.map((item) =>
                 <Product key={item.id} item={item} />
-            ))}
+            ) : products.slice(0, 8).map((item) =>
+                <Product item={item} key={item.id} />
+            )}
         </Container>
     )
 }
